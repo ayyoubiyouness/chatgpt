ec71942f587d94ce9ab06304373317bb9b70b8b7
Ayyoubi <114993701+ayyoubiyouness@users.noreply.github.com>
2024-07-26 09:50:39 +0200

    updates related to bcryptjs

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 export const register = async (req, res) => {
     try {
